8894959d81273ded9ea42da9520dcf1d32fd9d19
weapons1990 <286582326@qq.com>
2015-09-04 12:43:12 +0800

    init

---
+++ StatisticalFault/StatisticalFault.Service/AmmeterFault/AmmeterFaultService.cs
@@ -0,0 +1,37 @@
+﻿using SqlServerDataAdapter;
+using StatisticalFault.Infrastruture.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+
+namespace StatisticalFault.Service.AmmeterFault
+{
+    public class AmmeterFaultService
+    {
+
+        public static DataTable GetAmmeterErrorData(string organizationId, string startTime, string endTime)
+        {
+            string connectionString = ConnectionStringFactory.NXJCConnectionString;
+            ISqlServerDataFactory dataFactory = new SqlServerDataFactory(connectionString);
+//            string mySql = @"select A.OrganizationID,A.vDate, A.Description
+//                                from system_Error A
+//                                where A.vDate>=@startTime
+//                                and A.vDate<=@endTime
+//                                and A.OrganizationID=@organizationId";
+//            SqlParameter[] parameters = { new SqlParameter("startTime", startTime), new SqlParameter("endTime", endTime),
+//                                        new SqlParameter("organizationId", organizationId)};
+            string mySql = @"select top(1000) A.vDate, A.Description
+                                from system_Error A
+                                where A.vDate>=@startTime
+                                and A.vDate<=@endTime
+                                order by A.vDate desc";
+            SqlParameter[] parameters = { new SqlParameter("startTime", startTime), new SqlParameter("endTime", endTime)};
+            DataTable table = dataFactory.Query(mySql, parameters);
+            return table;
+        }
+
+    }
+}
---
+++ StatisticalFault/StatisticalFault.Web/UI_AmmeterFault/js/page/AmmeterFaultQuery.js
@@ -0,0 +1,118 @@
+﻿
+
+$(function () {
+    InitDate();
+    loadDataGrid("first");
+});
+//初始化日期框
+function InitDate() {
+    var nowDate = new Date();
+    var beforeDate = new Date();
+    beforeDate.setDate(nowDate.getDate() - 1);
+    var nowString = nowDate.getFullYear() + '-' + (nowDate.getMonth() + 1) + '-' + nowDate.getDate() + " " + nowDate.getHours() + ":" + nowDate.getMinutes() + ":" + nowDate.getSeconds();
+    var beforeString = beforeDate.getFullYear() + '-' + (beforeDate.getMonth()+1) + '-' + beforeDate.getDate() + " 00:00:00";
+    $('#startDate').datetimebox('setValue', beforeString);
+    $('#endDate').datetimebox('setValue', nowString);
+}
+
+
+function loadDataGrid(type, myData) {
+    if ("first" == type) {
+        $("#Windows_Report").datagrid({
+            striped: true,
+            rownumbers: true,
+            singleSelect: true,
+            fit: true,
+            pagination: true,
+            pageSize: 50,
+            toolbar:'#toolbar_ReportTemplate',
+            columns: [[
+                        { field: 'vDate', title: '故障时间', width: 150 },
+		                { field: 'Description', title: '故障描述', width: 500 }
+            ]]
+        });
+    }
+    else {
+        $("#Windows_Report").datagrid({ loadFilter: pagerFilter }).datagrid('loadData', myData["rows"]);
+    }
+}
+
+function onOrganisationTreeClick(node) {
+
+    // 仅能选中分厂级别
+    // 即组织机构ID的层次码 = 5
+
+    //if (node.id.length != 5) {
+    //    $.messager.alert('提示', '仅能选择分厂级别。');
+    //    return;
+    //}
+
+    // 设置组织机构ID
+    // organizationId为其它任何函数提供当前选中的组织机构ID
+
+    $('#organizationId').val(node.OrganizationId);
+
+    // 设置组织机构名称
+    // 用于呈现，在界面上显示当前的组织机构名称
+
+    $('#productLineName').textbox('setText', node.text);
+}
+
+
+function query() {
+    // 获取组织机构ID
+    var organizationId = $('#organizationId').val();
+
+    if (organizationId == '') {
+        $.messager.alert('提示', '请先选择需要分析的组织机构。');
+        return;
+    }
+
+    // 获取起止时间段
+    var startTime = $('#startDate').datebox('getValue');
+    var endTime = $('#endDate').datebox('getValue');
+
+
+    $.ajax({
+        type: "POST",
+        url: "AmmeterFaultQuery.aspx/GetReportData",
+        data: "{organizationId:'" + organizationId + "',startTime:'" + startTime + "',endTime:'" + endTime + "'}",
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (msg) {
+            var m_msg = JSON.parse(msg.d);
+            loadDataGrid("last", m_msg);
+        }
+    });
+}
+
+//datagrid最下面分页栏使用
+function pagerFilter(data) {
+    if (typeof data.length == 'number' && typeof data.splice == 'function') {	// is array
+        data = {
+            total: data.length,
+            rows: data
+        }
+    }
+    var dg = $(this);
+    var opts = dg.datagrid('options');
+    var pager = dg.datagrid('getPager');
+    pager.pagination({
+        onSelectPage: function (pageNum, pageSize) {
+            opts.pageNumber = pageNum;
+            opts.pageSize = pageSize;
+            pager.pagination('refresh', {
+                pageNumber: pageNum,
+                pageSize: pageSize
+            });
+            dg.datagrid('loadData', data);
+        }
+    });
+    if (!data.originalRows) {
+        data.originalRows = (data.rows);
+    }
+    var start = (opts.pageNumber - 1) * parseInt(opts.pageSize);
+    var end = start + parseInt(opts.pageSize);
+    data.rows = (data.originalRows.slice(start, end));
+    return data;
+}
